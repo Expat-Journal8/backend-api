0cd9a6481dbcda06727936f87c7a0d6338a31ed7
lex-marie790 <lexi.marie790@gmail.com>
2020-08-19 21:21:17 -0700

    index.js, server.js, db-congif.js, knexfile.js,package.json completed

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,53 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/expats.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      },
     }
   },
 
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds'
     }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds'
     }
   }
 
