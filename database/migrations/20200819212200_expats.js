78ecc549484c9fc077be80cf7de2afd5a2b6f886
lex-marie790 <lexi.marie790@gmail.com>
2020-08-20 19:08:00 -0700

    migration data & knex cleaner completed

---
+++ database/migrations/20200819212200_expats.js
@@ -0,0 +1,67 @@
+
+exports.up = function(knex) {
+  return(
+    knex.schema
+        .createTable('users', tbl => {
+            tbl.increments();
+            tbl.string('username', 128)
+                .unique()
+                .notNullable();
+            tbl.string('password', 256)
+                .notNullable();
+            tbl.text('firstName', 128)
+                .notNullable();
+            tbl.text('lastName', 128)
+                .notNullable();
+            tbl.string('gender', 128)
+                .notNullable();
+            tbl.integer('age')
+                .notNullable();
+            tbl.string('email', 128)
+                .notNullable()
+                .unique();
+        })
+        .createTable('stories',tbl => {
+            tbl.increments();
+            tbl.string('storyName', 128)
+                .notNullable();
+            tbl.string('storyCity', 128)
+                .notNullable();
+            tbl.string('storyCountry', 128)
+                .notNullable();
+            tbl.date('storyDate')
+                .notNullable();
+            tbl.text('storyDesc')
+                .notNullable();
+            tbl.integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('users.id')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        })
+        .createTable('photos', tbl => {
+            tbl.increments();
+            tbl.string('photoLink', 255)
+                .notNullable();
+            tbl.text('photDesc')
+                .notNullable();
+            tbl.date('photoDate');
+            tbl.integer('stories_id')
+                .unsigned()
+                .notNullable()
+                .references('stories.id')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
+    )
+};
+
+exports.down = function(knex) {
+  return (
+      knex.schema
+        .dropTableIfExists('photos')
+        .dropTableIfExists('stories')
+        .dropTableIfExists('users')
+  );
+};
