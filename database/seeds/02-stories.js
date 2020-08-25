dcea2fe3fdf98b452b12d8b6ab0be826f05d36b4
lex-marie790 <lexi.marie790@gmail.com>
2020-08-24 22:39:48 -0700

    updated seed

--- database/seeds/02-stories.js
+++ database/seeds/02-stories.js
@@ -1,8 +1,7 @@
-
 exports.seed = function(knex) {
-      return knex('stories').insert([
-        {storyName: 'Chinatown', storyCity: 'Bangkok', storyCountry: 'Thailand', storyDesc:'Out on a nightly excursion looking for something to eat', user_id: 1,},
-        {storyName: 'Sanctuary of Truth', storyCity: 'Pattaya City', storyCountry: 'Thailand', storyDesc: 'A daytime excursion visiting some local places.', user_id: 2,},
-        {storyName: 'Beach in Pattaya', storyCity: 'Pattaya City', storyCountry: 'Thailand', storyDesc: 'Taking a must needed rest after a weekend of fun in Pattaya', user_id: 3,}
-      ]);
+  return knex('stories').insert([
+    {storyName: 'Chinatown', storyCity: 'Bangkok', storyCountry: 'Thailand', storyDesc:'Out on a nightly excursion looking for something to eat', user_id: 1,},
+    {storyName: 'Sanctuary of Truth', storyCity: 'Pattaya City', storyCountry: 'Thailand', storyDesc: 'A daytime excursion visiting some local places.', user_id: 2,},
+    {storyName: 'Beach in Pattaya', storyCity: 'Pattaya City', storyCountry: 'Thailand', storyDesc: 'Taking a must needed rest after a weekend of fun in Pattaya', user_id: 3,}
+  ]);
 };
--- database/seeds/03-photos.js
+++ database/seeds/03-photos.js
@@ -1,8 +1,7 @@
-
 exports.seed = function(knex) {
-      return knex('photos').insert([
-        -{photoLink: 'https://i.ibb.co/DVN5Lnx/20200322-213304.jpg', photoDesc: 'Out for dinner', stories_id: 1,},
-        {photoLink: 'https://i.ibb.co/8dXhn81/20200809-204009.jpg', photoDesc: 'Visiting the Sanctuary of Truth in Pattaya', stories_id: 2,},
-        {photoLink: 'https://i.ibb.co/RTZNzfX/20200821-093310.jpg', photoDesc: 'Enjoy a moment of relaxation in Pattaya', stories_id: 3,}
-      ]);
+  return knex('photos').insert([
+    {photoLink: 'https://i.ibb.co/DVN5Lnx/20200322-213304.jpg', photoDesc: 'Out for dinner', stories_id: 1,},
+    {photoLink: 'https://i.ibb.co/8dXhn81/20200809-204009.jpg', photoDesc: 'Visiting the Sanctuary of Truth in Pattaya', stories_id: 2,},
+    {photoLink: 'https://i.ibb.co/RTZNzfX/20200821-093310.jpg', photoDesc: 'Enjoy a moment of relaxation in Pattaya', stories_id: 3,}
+  ]);
 };
