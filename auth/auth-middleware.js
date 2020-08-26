38f74fd46983e7742859c912d62fb218c444f25a
lex-marie790 <lexi.marie790@gmail.com>
2020-08-26 16:23:47 -0700

    made edit to auth-middleware

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets.js');
+const { secrets } = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers;
+        const { token } = req.headers;
         if (token) {
-            jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+            jwt.verify(token, secrets, (err, decodedToken) => {
                 if (err) {
                     res.status(401).json({
                         message: 'You are not authorized for this'
